456d7366d2caac39e71e26e9854aef7695267808
kirilikl <kirill.ahremenko@gmail.com>
2025-05-21 18:30:01 +0300

    qwe

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let obj = document.querySelector(".obj")
+let curScore = document.querySelector(".curScore")
+let numCurScore = 0
 
+obj.addEventListener("click", function(){
+    numCurScore++
+    curScore.innerHTML = "Очки: "+numCurScore
+    anime({
+        targets: ".obj",
+        scale: 0.92,
+        duration:100
+    }).finished.then(()=>{
+        anime({
+            targets: ".obj",
+            scale: 1,
+            duration: 100
+        })
+    })
+})
